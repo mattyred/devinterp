fa792d7427bbe3d501dcebfb29d63e22cdf47f8b
svwingerden <stanvanwingerden@gmail.com>
2025-01-24 18:16:22 +0000

    hotfix formatting

--- src/devinterp/vis_utils.py
+++ src/devinterp/vis_utils.py
@@ -26,6 +26,7 @@ class SweepConfig:
     beta_range: List[float]
     llc_estimator: Callable
     llc_estimator_kwargs: dict
+
     def __init__(self, epsilon_range, beta_range, llc_estimator, llc_estimator_kwargs):
         self.epsilon_range = epsilon_range
         self.beta_range = beta_range
